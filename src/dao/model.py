cfa068043ccc2021ec8e2253cb69a9ace6024c07
mizuthx <mizuthx@proton.me>
2025-10-17 20:50:33 -0300

    model edit

--- src/dao/model.py
+++ src/dao/model.py
@@ -1,13 +1,8 @@
 # silly script, only for model_chx() of database
-model:dict = {
-    'model_base': ("""
+base_chx:str = ("""
 SELECT COUNT(*) as tablas_existentes
 FROM information_schema.TABLES 
 WHERE TABLE_SCHEMA = DATABASE() 
 AND TABLE_NAME IN (?, ?);
-"""),
+""")
     
-    'model_data': ("empleado", 'telefono'),
-    
-    'model_views': ""
-}
