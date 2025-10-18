00600a599088c91d2b715e15717fcc4a24f967bb
mizuthx <mizuthx@proton.me>
2025-10-17 22:51:18 -0300

    update index.py

--- src/dto/index.py
+++ src/dto/index.py
@@ -1,3 +1,11 @@
 
 class Empleado:
-    pass
+    def __init__(self, nombre:str, numero_tel:int, direccion:str, email:str, inicio_contrato, salario:int):
+        self.nombre = nombre
+        self.numero_tel = numero_tel
+        self.direccion = direccion
+        self.email = email 
+        self.inicio_contrato = inicio_contrato
+        self.salario = salario
+        
+        
