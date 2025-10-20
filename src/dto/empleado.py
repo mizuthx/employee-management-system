7b074560c35cd3b2a1bab78a7fb980644d562f42
mizuthx <mizuthx@proton.me>
2025-10-20 13:57:10 -0300

    re struc

--- src/dto/empleado.py
+++ src/dto/empleado.py
@@ -1,5 +1,4 @@
-
-class Empleado:
+class EmpleadoDTO:
     def __init__(self, nombre:str, numero_tel:int, direccion:str, email:str, inicio_contrato, salario:int):
         self.nombre = nombre
         self.numero_tel = numero_tel
@@ -7,5 +6,5 @@ class Empleado:
         self.email = email 
         self.inicio_contrato = inicio_contrato
         self.salario = salario
-        
-        
+    def __str__(self) -> str:
+        return f"{self.nombre}"
