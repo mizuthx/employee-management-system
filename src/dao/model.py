bea0c228c24df3b5c983463c572b0442d695951e
mizuthx <mizuthx@proton.me>
2025-10-21 09:57:24 -0300

    update model script

--- src/dao/model.py
+++ src/dao/model.py
@@ -3,79 +3,86 @@ base_chx:str = ("""
 SELECT COUNT(*) as tablas_existentes
 FROM information_schema.TABLES 
 WHERE TABLE_SCHEMA = DATABASE() 
-AND TABLE_NAME IN (?, ?, ?, ?);
+AND TABLE_NAME IN (?, ?, ?, ?, ?);
 """)
     
 base:list = [
-    # Crear tabla departamentos
-    """CREATE TABLE IF NOT EXISTS departamentos
+    """CREATE TABLE IF NOT EXISTS roles
     (
-      id_departamento INT NOT NULL AUTO_INCREMENT,
-      nombre VARCHAR(45) NOT NULL,
-      descripcion VARCHAR(500) NULL,
-      PRIMARY KEY (id_departamento)
-    )""",
+      id_rol INT         NOT NULL AUTO_INCREMENT,
+      nombre VARCHAR(20) NOT NULL,
+      PRIMARY KEY (id_rol)
+    );""",
     
-    # Constraint unique para departamentos.nombre
-    """ALTER TABLE departamentos
-       ADD CONSTRAINT UQ_nombre_dept UNIQUE (nombre)""",
-    
-    # Crear tabla empleados
     """CREATE TABLE IF NOT EXISTS empleados
     (
-      id_empleado INT NOT NULL AUTO_INCREMENT,
-      nombre VARCHAR(20) NOT NULL,
-      primer_apellido VARCHAR(20) NOT NULL,
-      segundo_apellido VARCHAR(20) NULL,
-      telefono VARCHAR(11) NOT NULL,
-      email VARCHAR(50) NOT NULL,
-      inicio_contrato DATE NOT NULL,
-      salario INT NOT NULL,
-      id_departamento INT NOT NULL,
+      id_empleado      INT         NOT NULL AUTO_INCREMENT,
+      nombre           VARCHAR(20) NOT NULL,
+      primer_apellido  VARCHAR(20) NOT NULL,
+      segundo_apellido VARCHAR(20) NULL    ,
+      id_rol           INT         NOT NULL,
+      telefono         VARCHAR(11) NOT NULL,
+      email            VARCHAR(50) NOT NULL,
+      inicio_contrato  DATE        NOT NULL,
+      salario          INT         NOT NULL,
       PRIMARY KEY (id_empleado)
-    )""",
+    );""",
     
-    # Crear tabla proyectos
+    """CREATE TABLE IF NOT EXISTS departamentos
+    (
+      id_departamento INT          NOT NULL AUTO_INCREMENT,
+      id_empleado     INT          NOT NULL,
+      nombre          VARCHAR(45)  NOT NULL,
+      descripcion     VARCHAR(500) NULL    ,
+      PRIMARY KEY (id_departamento)
+    );""",
+    
     """CREATE TABLE IF NOT EXISTS proyectos
     (
-      id_proyecto INT NOT NULL AUTO_INCREMENT,
-      id_empleado INT NOT NULL,
-      nombre VARCHAR(45) NOT NULL,
-      descripcion VARCHAR(500) NOT NULL,
-      fecha_inicio DATE NOT NULL,
+      id_proyecto   INT          NOT NULL AUTO_INCREMENT,
+      id_empleado   INT          NOT NULL,
+      nombre        VARCHAR(45)  NOT NULL,
+      descripticion VARCHAR(500) NOT NULL,
+      fecha_inicio  DATE         NOT NULL,
       PRIMARY KEY (id_proyecto)
-    )""",
+    );""",
     
-    # Constraint unique para proyectos.nombre
-    """ALTER TABLE proyectos
-       ADD CONSTRAINT UQ_nombre_proy UNIQUE (nombre)""",
-    
-    # Crear tabla registros
     """CREATE TABLE IF NOT EXISTS registros
     (
-      id_registro INT NOT NULL AUTO_INCREMENT,
-      id_empleado INT NOT NULL,
-      fecha_inicio DATE NOT NULL,
-      horas INT NOT NULL,
-      descripcion VARCHAR(500) NOT NULL,
+      id_registro  INT          NOT NULL AUTO_INCREMENT,
+      id_empleado  INT          NOT NULL,
+      fecha_inicio DATE         NOT NULL,
+      horas        INT          NOT NULL,
+      descripcion  VARCHAR(500) NOT NULL,
       PRIMARY KEY (id_registro)
-    )""",
+    );""",
     
-    # Foreign Key: empleados -> departamentos
-    """ALTER TABLE empleados
-       ADD CONSTRAINT FK_departamentos_TO_empleados
-       FOREIGN KEY (id_departamento)
-       REFERENCES departamentos (id_departamento)""",
+    """ALTER TABLE roles
+      ADD CONSTRAINT UQ_nombre_roles UNIQUE (nombre);""",
     
-    # Foreign Key: proyectos -> empleados
+    """ALTER TABLE departamentos
+      ADD CONSTRAINT UQ_nombre_departamentos UNIQUE (nombre);""",
+    
     """ALTER TABLE proyectos
-       ADD CONSTRAINT FK_empleados_TO_proyectos
-       FOREIGN KEY (id_empleado)
-       REFERENCES empleados (id_empleado)""",
+      ADD CONSTRAINT UQ_nombre_proyectos UNIQUE (nombre);""",
     
-    # Foreign Key: registros -> empleados
+    """ALTER TABLE empleados
+      ADD CONSTRAINT FK_roles_TO_empleados
+        FOREIGN KEY (id_rol)
+        REFERENCES roles (id_rol);""",
+    
+    """ALTER TABLE departamentos
+      ADD CONSTRAINT FK_empleados_TO_departamentos
+        FOREIGN KEY (id_empleado)
+        REFERENCES empleados (id_empleado);""",
+    
+    """ALTER TABLE proyectos
+      ADD CONSTRAINT FK_empleados_TO_proyectos
+        FOREIGN KEY (id_empleado)
+        REFERENCES empleados (id_empleado);""",
+    
     """ALTER TABLE registros
-       ADD CONSTRAINT FK_empleados_TO_registros
-       FOREIGN KEY (id_empleado)
-       REFERENCES empleados (id_empleado)"""
+      ADD CONSTRAINT FK_empleados_TO_registros
+        FOREIGN KEY (id_empleado)
+        REFERENCES empleados (id_empleado);"""
 ]
